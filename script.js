c64793a420a1f0b08b142212e071334b4e25624b
Startup4me <girishnalawade1145@gmail.com>
2025-09-27 22:08:28 +0530

    Added Grid & date part

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-let todoList=[];
+let todoList=[
+  {
+    item:'Make Todo App',
+    dueDate:'27/9/2025'
+  },
+  {
+    item:'Be Patience',
+    dueDate:'27/9/2025'
+  }
+];
 displayItems();
 
 function addTodo(){
  let inputElement= document.getElementById("todo-input");
 let todoItem= inputElement.value;
-
-todoList.push(todoItem);
+let dateElement=document.getElementById('todo-date');
+let todoDate=dateElement.value;
+todoList.push({item:todoItem ,dueDate:todoDate});
 inputElement.value='';
+dateElement.value='';
 displayItems();
 }
 
 function displayItems(){
-  let displayElement=document.getElementById("todo-item");
-  displayElement.innerText='';
+  let containerElement=document.querySelector('.todo-container');
+  let newHtml=' ';
   for(let i=0;i<todoList.length;i++){
-    displayElement.innerText=    displayElement.innerText+ "\n"+todoList[i];
-  }
+ let {item,dueDate}=todoList[i];
+    newHtml+=`
+  
+    <span>${item}</span>
+    <span>${dueDate}</span>
+    <button onclick="todoList.splice(${i},1);
+    displayItems(); ">Delete</button>
+
+    `;
+     }containerElement.innerHTML=newHtml;
 }
 
 
